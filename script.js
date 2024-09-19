055b7e334e64e95a957a38aa0d26f157a4b9deae
TheCyberAyo <ndamaseayongezwa@gmail.com>
2024-09-19 15:53:36 +0200

    added more practice examples

--- script.js
+++ script.js
@@ -44,3 +44,121 @@ if (canVote === true){
 else{
     console.log("not aligible to vote");
 }
+
+//If else example
+if (day === "Monday"){
+    console.log("Start of the workweek;");
+}else if(day === Friday){
+    console.log("Almost the weekend")
+} else {
+    console.log("Just another day")
+}
+
+//switch statement
+switch (day){
+    case "Monday":
+        console.log("Start of the week");
+        break;
+    case "Friday":
+        console.log("last day of day of the week");
+        break;
+    case "Wednesday":
+        console.log("Midweek Grind");
+        break;
+    default:
+        console.log("Just another day");
+}
+
+//declaring a function
+function greet(name) {
+    return "Hello, " + name + "!"
+}
+
+//calling a function
+
+let message = greet("Alice")
+console.log(message) //output: Hello, Alice!
+
+
+//scope example
+let globalVar= "I am global"
+
+function checkScope(){
+    let localVar = "I'm Local";
+    console.log(globalVar); //accessible here although out of here
+    console.log(localVar); //accessible because it is found here
+}
+
+checkScope();
+
+console.log(globalVar); //acessible here
+console.log(localVar); //not accessible beacause it is only local to the fuction that contains it.
+
+
+
+//DOM AND EVERYTHING RELATED 
+//selecting by id and modifying its content
+
+let heading = document.getElementById("main-heading");
+heading.textContent = "Love is a beautiful thing";
+
+//changing the style of a   element 
+let blueDay = document.querySelector(".landing-page");
+blueDay.style.backgroundColor = "blue";
+
+//adding a new class to an element
+blueDay.classList.add("day-description");
+
+// creating a new element and appending to the DOM
+let newElem = document.createElement("section");
+newElem.textContent = "This is an element added through DOM manipulation";
+document.body.appendChild(newElem); //I could use appendchild to add just one element to a "place"
+
+let newElem2 = documents.createElement("aside");
+newElem2.textContent = "A good day to have a good day";
+document.body.appendChild(newElem2); // result: A good day to have a good day
+
+//alternative to the above - if i want the above to appear as siblings
+
+document.body.append(newElem, newElem2); //result: This is ament added through DOM manipulation.
+                                         //        A good day to have a good day.
+
+
+// EVENTS(Events, eventListeners)
+
+//EVENT LISTENERS (click, mouseover, keydown)
+//Inline Event Listeners --- are those that can be added into an an HTML page, like buttons.
+// e.g <button onclick ="Do SOMETHING"
+
+
+// COMMON EVENT TYPES
+    //Mouse Events (click, dbclick, mouseover, mouseout, mouseup, ousedown)
+    //Keyboard Events (keydown, keyup, keypress)
+    //Form Events(submit, change, focus, blur)
+    //Document/Window Events(load, resize, scroll, DOMContent Loaded)
+
+//selecting a button
+let button = document.querySelector("#myButton");
+//add a click listener to the button
+button.addEventListener("click", function(event){
+    //change button text when clicked
+    button.textContent = "Button Clicked!"; 
+
+    //log event details to the console
+    console.log("Event Type:", event.type);
+    console.log("Button Text", event.target.textContent);
+
+});
+
+//Adding mouseover event listener to change background-color
+button.addEventListener("mouseover", function() {
+    button.style.backgroundColor="lightblue";
+});
+
+button.addEventListener("mouseout", function() {
+    button.style.backgroundColor="";
+});
+
+
+
+
